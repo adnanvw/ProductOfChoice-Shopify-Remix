e8f500b931853bf623061e537a98c1bdb88c9de3
adnan <adnan1.vw@gmail.com>
2023-12-16 09:15:33 +0530

    initial commit

---
+++ app/routes/app._index.jsx
@@ -0,0 +1,138 @@
+import * as React from "react";
+import * as P from '@shopify/polaris';
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { authenticate } from "../shopify.server";
+
+export const loader = async ({ request }) => {
+  const response = await fetch("https://dummyjson.com/products").then(response => response.json());
+  return response.products;
+};
+
+export const action = async ({ request }) => {
+  const { admin } = await authenticate.admin(request);
+  const color = ["Red", "Orange", "Yellow", "Green"][
+    Math.floor(Math.random() * 4)
+  ];
+  const response = await admin.graphql(
+    `#graphql
+      mutation populateProduct($input: ProductInput!) {
+        productCreate(input: $input) {
+          product {
+            id
+            title
+            handle
+            status
+            variants(first: 10) {
+              edges {
+                node {
+                  id
+                  price
+                  barcode
+                  createdAt
+                }
+              }
+            }
+          }
+        }
+      }`,
+    {
+      variables: {
+        input: {
+          title: `${color} Snowboard`,
+          variants: [{ price: Math.random() * 100 }],
+        },
+      },
+    }
+  );
+  const responseJson = await response.json();
+
+  return json({
+    product: responseJson.data.productCreate.product,
+  });
+};
+
+export default function Index() {
+  const products = useLoaderData();
+
+  const resourceName = { singular: 'product', plural: 'products' };
+  const { selectedResources, allResourcesSelected, handleSelectionChange } = P.useIndexResourceState(products);
+
+  const rowMarkup = products.map(
+    (
+      { id, title, brand, stock, category, price, thumbnail },
+      index,
+    ) => (
+      <P.IndexTable.Row
+        id={id}
+        key={id}
+        selected={selectedResources.includes(id)}
+        position={index}
+      >
+        <P.IndexTable.Cell>
+          <P.Text variant="bodyMd" fontWeight="bold" as="span">
+            {id}
+          </P.Text>
+        </P.IndexTable.Cell>
+        <P.IndexTable.Cell>
+          <P.InlineStack blockAlign="center" gap="400">
+            <P.Thumbnail
+              source={thumbnail}
+              size="large"
+              alt="image"
+            />
+            <P.Text as="span" alignment="end">
+              <P.Link url={`product/${id}`}>{title}</P.Link>
+            </P.Text>
+          </P.InlineStack>
+        </P.IndexTable.Cell>
+        <P.IndexTable.Cell>{brand}</P.IndexTable.Cell>
+        <P.IndexTable.Cell>
+          <P.Text as="span" alignment="end">
+            {stock}
+          </P.Text>
+        </P.IndexTable.Cell>
+        <P.IndexTable.Cell>
+          <P.Text as="span" alignment="end">
+            {category}
+          </P.Text>
+        </P.IndexTable.Cell>
+        <P.IndexTable.Cell>
+          <P.Text as="span" alignment="end">
+            ${price}
+          </P.Text>
+        </P.IndexTable.Cell>
+      </P.IndexTable.Row>
+    ),
+  );
+
+  return (
+    <P.Page fullWidth>
+      <P.Layout>
+        <P.Layout.Section>
+          <P.LegacyCard>
+            <P.IndexTable
+              resourceName={resourceName}
+              itemCount={products.length}
+              selectedItemsCount={
+                allResourcesSelected ? 'All' : selectedResources.length
+              }
+              onSelectionChange={handleSelectionChange}
+              headings={[
+                { title: '#' },
+                { title: 'Product' },
+                { title: 'Brand' },
+                { title: 'Stock', alignment: 'end' },
+                { title: 'Category', alignment: 'end' },
+                { title: 'Price', alignment: 'end' },
+              ]}
+              selectable={false}
+            >
+              {rowMarkup}
+            </P.IndexTable>
+          </P.LegacyCard>
+        </P.Layout.Section>
+      </P.Layout>
+    </P.Page>
+  );
+}
---
+++ app/routes/app.product.$id.jsx
@@ -0,0 +1,142 @@
+import * as P from '@shopify/polaris';
+import { useLoaderData, useSubmit, useActionData } from "@remix-run/react";
+import { authenticate } from '../shopify.server';
+import { useEffect } from 'react';
+
+export async function loader({ request }) {
+    const url = new URL(request.url);
+    const product_id = url.pathname.replace("/app/product/", "");
+
+    const response = await fetch(`https://dummyjson.com/products/${product_id}`).then(response => response.json());
+    return response;
+}
+
+export async function action({ request }) {
+    const { admin } = await authenticate.admin(request);
+    const body = await request.formData();
+    const image_arr = [];
+
+    for (let i = 0; i < body.get("images").split(",").length; i++) {
+        image_arr.push({
+            originalSource: body.get("images").split(",")[i],
+            alt: body.get("title"),
+            mediaContentType: "IMAGE"
+        });
+    }
+
+    const response = await admin.graphql(
+        `#graphql
+        mutation CreateProductWithNewMedia($input: ProductInput!, $media: [CreateMediaInput!]) {
+            productCreate(input: $input, media: $media) {
+                product {
+                    id
+                    title
+                }
+                userErrors {
+                    field
+                    message
+                }
+            }
+        }`,
+        {
+            variables: {
+                input: {
+                    title: body.get("title"),
+                    bodyHtml: `<p>${body.get("description")}</p>`,
+                    tags: [body.get("category")],
+                    vendor: body.get("brand"),
+                    variants: [
+                        {
+                            price: Number(body.get("price")),
+                        }
+                    ],
+                },
+                "media": image_arr
+            },
+        }
+    );
+
+    return true;
+}
+
+export default function ProductDetails() {
+    const product = useLoaderData();
+    const submit = useSubmit();
+    const status = useActionData();
+    console.log("status", status);
+
+    useEffect(() => {
+        if (status == true) {
+            alert("Product successfully added to the store.");
+        }
+    }, [status]);
+
+    return (
+        <P.Page
+            backAction={{ content: 'Go Back', url: '/app' }}
+            title={product.title}
+            primaryAction={
+                <P.Button
+                    variant="primary"
+                    onClick={handleSubmit}>
+                    Add Product to Store
+                </P.Button>
+            }
+        >
+            <P.LegacyCard title="Product Details" sectioned>
+                <P.TextField
+                    label="Title"
+                    value={product.title}
+                    readOnly
+                    autoComplete="off"
+                />
+                <br />
+                <P.TextField
+                    label="Description"
+                    value={product.description}
+                    readOnly
+                    autoComplete="off"
+                />
+                <br />
+                <P.TextField
+                    label="Brand"
+                    value={product.brand}
+                    readOnly
+                    autoComplete="off"
+                />
+                <br />
+                <P.TextField
+                    label="Category"
+                    value={product.category}
+                    readOnly
+                    autoComplete="off"
+                />
+                <br />
+                <P.TextField
+                    prefix="$"
+                    label="Price"
+                    value={product.price}
+                    readOnly
+                    autoComplete="off"
+                />
+                <br />
+            </P.LegacyCard>
+
+            <P.LegacyCard title="Product Media" sectioned>
+                {
+                    product.images.map((img, index) => {
+                        return <img key={index} src={img} width={"50%"} alt="img" />
+                    })
+                }
+            </P.LegacyCard>
+        </P.Page >
+    )
+
+    async function handleSubmit() {
+
+        submit(
+            { ...product },
+            { replace: true, method: "POST" }
+        );
+    }
+}
